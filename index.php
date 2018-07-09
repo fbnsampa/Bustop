4af232950f9884585f8f82fe85eeb7cca9b981fe
Fernando Gavinhos <s_fernando2007@hotmail.com>
2018-07-09 13:45:40 -0300

    [Fix] Add style config

--- index.php
+++ index.php
@@ -13,33 +13,37 @@
         
         <!-- adicionar CSS Personalizado-->
         <link href="css/style.css" rel="stylesheet" media="screen">
+        <style>
+        /* Set the size of the div element that contains the map */
+        #map {
+            height: 400px;  /* The height is 400 pixels */
+            width: 100%;  /* The width is the width of the web page */
+        }
+        </style>
     </head>
     <body>
         <h1>Olá Mundo! (branch: gmaps.gavinhos)</h1>
-        <div id="map"></div>
-        <script>
+    <div id="map"></div>
+    <script>
+        // Initialize and add the map
         function initMap() {
-            var myLatLng = {lat: -25.363, lng: 131.044};
-
-            // Create a map object and specify the DOM element
-            // for display.
-            var map = new google.maps.Map(document.getElementById('map'), {
-            center: myLatLng,
-            zoom: 4
-            });
-
-            // Create a marker and set its position.
-            var marker = new google.maps.Marker({
-            map: map,
-            position: myLatLng,
-            title: 'TestMap - Australia'
-            });
+        // The location of Uluru
+        var uluru = {lat: -25.344, lng: 131.036};
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+            document.getElementById('map'), {zoom: 4, center: uluru});
+        // The marker, positioned at Uluru
+        var marker = new google.maps.Marker({position: uluru, map: map});
         }
-        </script>
-        
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyADUk4Y_cwF5ObmSJTMZ8w3L2g6E-GZEE4
-&callback=initMap" async defer>
-        </script>
+    </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyADUk4Y_cwF5ObmSJTMZ8w3L2g6E-GZEE4&callback=initMap">
+    </script>
 
         <script src="js/jquery-3.2.1.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>

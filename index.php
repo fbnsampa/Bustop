7745916d6d7e799b2cdf905e026cdc07e92bade0
fabiano <fabiano@fabiano-Mint>
2018-07-09 04:22:43 -0300

    Pontapé incial
    Incluída a biblioteca do bootstrap e criado index.php com o código base
    "Ola Mundo".

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BusTOP</title>
+        
+        <!--define a viewport-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="utf-8">
+        
+        
+        <!-- adicionar CSS Bootstrap-->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        
+        <!-- adicionar CSS Personalizado-->
+        <link href="css/style.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <h1>Olá Mundo!</h1>
+
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>

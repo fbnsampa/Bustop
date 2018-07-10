6b29108c3473885927a3590f730506f0eca4ad65
Fernando Gavinhos <s_fernando2007@hotmail.com>
2018-07-10 00:59:29 -0300

    [Fix][Test] Use coordinates that exist

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
                         // Initialize and add the map
                         function initMap() {
                         var uluru = {lat: -25.344, lng: 131.036};
-                        var antiUluru = {lat: 131.036, lng: -25.344};
+                        var antiUluru = {lat: 25.344, lng: -131.036};
                         var map = new google.maps.Map(
                             document.getElementById('map'),
                             {zoom: 4, center: uluru}

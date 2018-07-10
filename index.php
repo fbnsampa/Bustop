ebb4b29c2d5c3b41447dfca1e25dc62e56ac9811
Fernando Gavinhos <s_fernando2007@hotmail.com>
2018-07-10 02:22:16 -0300

    [Update] Adjust map zoom

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
                         var uluru = {lat: -23.483486, lng: -46.501887};
                         var map = new google.maps.Map(
                             document.getElementById('map'),
-                            {zoom: 4, center: uluru}
+                            {zoom: 12, center: uluru}
                         );
                         var marker = new google.maps.Marker({position: uluru, map: map});
                         }
